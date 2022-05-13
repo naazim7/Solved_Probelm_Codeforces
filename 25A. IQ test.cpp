62c5049a98bcdb467ccba94f982f712b3cadc98b
Mobarak Hossain Nazim <59817491+naazim7@users.noreply.github.com>
2022-05-14 00:27:08 +0600

    Add files via upload

---
+++ 25A. IQ test.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n,ev=0,odd=0;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+    if(arr[i]%2==0) ev++;
+        else odd++;
+}
+if(ev>odd){
+    for(int i=0;i<n;i++){
+        if(arr[i]%2!=0)
+            cout<<i+1<<endl;
+    }
+}
+else {
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0)
+            cout<<i+1<<endl;
+    }
+}
+
+}
