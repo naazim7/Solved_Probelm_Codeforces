d41e0bf8fe4e78dacd8f2ea3f3744036b1f8626a
Mobarak Hossain Nazim <59817491+naazim7@users.noreply.github.com>
2022-07-14 00:25:54 +0600

    Add files via upload

---
+++ 268.C. Beautiful Sets of Points.cpp
@@ -0,0 +1,24 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+
+
+        int n,m,x,y;
+        cin>>x>>y;
+        m=min(x,y);
+        cout<<m+1<<endl;
+
+        for(int i=0;i<=m;i++){
+            cout<<i<<" "<<m-i<<endl;
+        }
+
+
+
+
+
+    return 0;
+}
