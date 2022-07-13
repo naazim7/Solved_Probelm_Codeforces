fbd904ffe16aace7ae9b47f76fdc706ff5149215
Mobarak Hossain Nazim <59817491+naazim7@users.noreply.github.com>
2022-07-14 00:07:28 +0600

    Create 268A. Games.cpp

---
+++ 268A. Games.cpp
@@ -0,0 +1,35 @@
+//Mobarak Hossain Nazim
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n;
+    cin>>n;
+    
+    int arr[n][2];
+
+    
+for(int i=0;i<n;i++){
+    for(int j=0;j<2;j++){
+        cin>>arr[i][j];
+    }
+    
+}   
+
+int cnt=0;
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(arr[i][1]==arr[j][0]){
+            cnt++;
+        }
+        
+    }
+    
+}
+cout<<cnt<<endl;
+
+
+    return 0;
+}
